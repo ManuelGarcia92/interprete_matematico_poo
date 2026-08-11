6e1b6beaf26b6dd7fe327535557efdff7cbc508d
ManuelGarcia92 <jmgb7738@gmail.com>
2026-08-11 13:24:52 -0400

    se añadio el algoritmo shuting yard para los calculos

--- pruebas.py
+++ pruebas.py
@@ -1,5 +1,55 @@
+OPERACIONES = {
+    "**": lambda x, y: x ** y,
+    "$" : lambda x, y: x ** (1 / y),
+    "*" : lambda x, y: x * y,
+    "/" : lambda x, y: x / y,
+    "//": lambda x, y: x // y,
+    "+" : lambda x, y: x + y,
+    "-" : lambda x, y: x - y
+}
+
+prioridad = {"+":1,"-":1,"*":2,"/":2,"**":3}
+
+asociatividad = {"+":"L","-":"L","*":"L","/":"L","**":"R"}
+
+def shuting_yard(tokens, prioridad, asociatividad):
+    cola_salida = []
+    pila_operadores = []
+    for token in tokens:
+        if token.replace(".","", 1).isdigit() or (token.startswith("-") and len(token) > 1):
+            cola_salida.append(token)
+        elif token == "(":
+            pila_operadores.append(token)
+        elif token == ")":
+            while pila_operadores and pila_operadores[-1] != "(":
+                cola_salida.append(pila_operadores.pop())   
+            if pila_operadores:
+                pila_operadores.pop()         
+        elif token in prioridad:
+            while (pila_operadores and pila_operadores[-1] != "(" and
+                    (prioridad[pila_operadores[-1]] > prioridad[token] or 
+                     (prioridad[pila_operadores[-1]] == prioridad[token] and asociatividad[token] == "L"))):
+                cola_salida.append(pila_operadores.pop())
+            pila_operadores.append(token)
+    while pila_operadores:
+        cola_salida.append(pila_operadores.pop())
+    return cola_salida
+
+def evaluar_posfijo(lista_posfijo, operaciones):
+    pila_numeros = []
+    for token in lista_posfijo:
+        if token in operaciones:
+            b = float(pila_numeros.pop())
+            a = float(pila_numeros.pop())
+            resultado = operaciones[token](a, b)
+            pila_numeros.append(resultado)
+        else:
+            pila_numeros.append(token)
+    return pila_numeros[0] if pila_numeros else 0
+
 espacio_variables = {}
-variables = ":x=2;:y=7"
+variables = input("Introduce las variables >>> : ").strip()
+operacion = input("Operación >>> : ").strip()
 buffer_nombre = ""
 buffer_valor = ""
 i = 0
@@ -17,5 +67,16 @@ while i < len(variables):
     espacio_variables[buffer_nombre] = buffer_valor
     buffer_nombre = "" ; buffer_valor = ""
     i += 1
-print(espacio_variables)
-            
+e = 0
+tokens_crudos = []
+while e < len(operacion):
+    if operacion[e] in espacio_variables:
+        tokens_crudos.append(espacio_variables[operacion[e]])
+        e += 1
+    else:
+        tokens_crudos.append(operacion[e])
+        e += 1  
+        
+expresion = shuting_yard(tokens_crudos, prioridad, asociatividad)
+resultado = evaluar_posfijo(expresion, OPERACIONES)
+print(resultado)

e1602571d3e71d39861cc9abd11635c48aae0f19
ManuelGarcia92 <jmgb7738@gmail.com>
2026-08-14 14:13:04 -0400

    pequeñas modificaciones en main

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from lexer import Lexer 
 from parser import Parser
-texto = "2*3+2+2*3/2/2*42/2+4-45"
-lexer = Lexer(texto)
-tokens = lexer.tokenizar()
-parser = Parser(tokens)
-try:
-    arbol = parser.expr()
-    resultado = arbol.evaluar()
-    print(resultado)
-except Exception as error:
-    print(error)
+while True:
+    texto = input(">>> : ")
+    if texto == "xyz":
+        break
+    lexer = Lexer(texto)
+    tokens = lexer.tokenizar()
+    parser = Parser(tokens)
+    try:
+        arbol = parser.expr()
+        resultado = arbol.evaluar()
+        print(resultado)
+    except Exception as error:
+        print(error)
 
 
